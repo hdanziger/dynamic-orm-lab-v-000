2cdb70a9f896957a72d7dafb8dc75d4d96611ff6
Hannah Danziger <hdanziger8@gmail.com>
2017-10-31 15:47:12 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,8 +26,6 @@ class InteractiveRecord
     attr_accessor col_name.to_sym
   end
 
-  #self.class.column_names.delete_if {|col| col == "id"}
-
   def initialize(options={})
   options.each do |property, value|
     self.send("#{property}=", value)

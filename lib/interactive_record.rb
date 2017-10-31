81e8f09cbc9240abb3eead1b3279c26f6eb66302
Hannah Danziger <hdanziger8@gmail.com>
2017-10-31 15:58:23 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -14,9 +14,8 @@ class InteractiveRecord
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-
     table_info.each do |column|
-      column_names << column["name"]
+      column_names << row["name"]
     end
 
     column_names.compact
